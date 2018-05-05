9380b6cb60fa93859c4f591e85a1b772dbd170ce
SK <skps2000@gmail.com>
2018-05-05 14:55:08 +0900

    haha

--- src/main/java/com/spring/common/HomeController.java
+++ src/main/java/com/spring/common/HomeController.java
@@ -19,21 +19,22 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET) 
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
-		
 		return "home";
 	}
 	
+	@RequestMapping(value = "/index", method = RequestMethod.GET) 
+	public String index(Locale locale, Model model) {
+		return "index";
+	}
+	
+	
+	
 }
+ 
